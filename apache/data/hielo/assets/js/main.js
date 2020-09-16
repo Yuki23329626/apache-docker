37a20dd50a677a956374466d24d19e8a0b4d7bc2
yukine23329626 <michael7105@gmail.com>
2020-09-16 16:34:19 +0800

    test

--- apache/data/hielo/assets/js/main.js
+++ apache/data/hielo/assets/js/main.js
@@ -284,7 +284,7 @@ var settings = {
 				.append('<a href="#menu" class="close"></a>')
 				.appendTo($body)
 				.panel({
-					delay: 1000,
+					delay: 100000,
 					hideOnClick: true,
 					hideOnSwipe: true,
 					resetScroll: true,
